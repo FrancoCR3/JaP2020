65bccee3bfbd181a1f609afd4bf32cae66ba23b0
FrancoCR3 <56952525+FrancoCR3@users.noreply.github.com>
2020-08-16 19:48:18 -0300

    tarea 1
    primera entrega

--- js/login.js
+++ js/login.js
@@ -2,5 +2,31 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+document.getElementById("boton").addEventListener("click", function(e){
+    let correo = document.getElementById("Elmail")
+    let contraseña = document.getElementById("laContraseña")
+    let todoCompleto = true;
+    
+    if (correo.value == ""){
+        todoCompleto = false;
+    }
 
+    if (contraseña.value == ""){
+        todoCompleto = false;
+    }
+
+    if (todoCompleto === true){
+        window.location = "inicio.html"
+    }
+
+    if(todoCompleto === false){
+        alert("Debe llenar los campos")
+    }
+}
+
+
+
+
+
+)
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,33 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var productosArray = [];
 
+function mostrarProductos(array){
+    let productos = "";
+    for(let i = 0; i < array.length; i++){
+        let producto = array[i];
+
+        productos += "<p>Nombre: " +  producto.name + "</p> <br>";
+        productos += "<p>Descripción: " +  producto.description + "</p><br>";
+        productos += "<p>Precio" + producto.cost + producto.currency + "</p><br><hr>";
+        
+    }
+    document.getElementById("losProductos").innerHTML = productos
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productosArray = resultObj.data;
+            mostrarProductos(productosArray);
+        }
+    })
 });
+
+
+
+
+
+
+
+
+
